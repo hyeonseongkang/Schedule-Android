fbfc30b5d4e30da2bc2a93c274e35adadf22eecb
hyeonseongkang <gt9697@naver.com>
2022-07-08 22:59:58 +0900

    git test

--- app/src/main/java/com/mirror/schedule_android/MainActivity.java
+++ app/src/main/java/com/mirror/schedule_android/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     public static String TAG = "MainActivity";
+    // git test
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
