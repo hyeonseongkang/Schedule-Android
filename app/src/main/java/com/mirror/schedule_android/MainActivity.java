42fab022032a38e17963b9ee764ca9e983c8d3c8
hyeonseongkang <gt9697@naver.com>
2022-07-09 20:42:41 +0900

    bottomNavigation 구현

--- app/src/main/java/com/mirror/schedule_android/MainActivity.java
+++ app/src/main/java/com/mirror/schedule_android/MainActivity.java
@@ -1,17 +1,70 @@
 package com.mirror.schedule_android;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static String TAG = "MainActivity";
-    // git test
+    public static final String TAG = "MainActivity";
+
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigation);
+        navigater(R.id.schedule);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                navigater(item.getItemId());
+                return true;
+            }
+        });
+    }
+
+    //BottomNavigation 페이지 변경
+    private void navigater(int id) {
+        String tag = String.valueOf(id);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        Fragment currentFragment = fragmentManager.getPrimaryNavigationFragment();
+        if (currentFragment != null) {
+            fragmentTransaction.hide(currentFragment);
+        }
+
+        Fragment fragment = fragmentManager.findFragmentByTag(tag);
+        if (fragment == null) {
+            if (id == R.id.schedule) {
+                fragment = new ScheduleFragment();
+
+            } else if (id == R.id.chatlist){
+
+                fragment = new ChatFragment();
+            }else {
+                fragment = new MapFragment();
+            }
+
+            fragmentTransaction.add(R.id.main_frame, fragment, tag);
+        } else {
+            fragmentTransaction.show(fragment);
+        }
+
+        fragmentTransaction.setPrimaryNavigationFragment(fragment);
+        fragmentTransaction.setReorderingAllowed(true);
+        fragmentTransaction.commitNow();
+
+
     }
 }
---
+++ app/src/main/java/com/mirror/schedule_android/MapFragment.java
@@ -0,0 +1,24 @@
+package com.mirror.schedule_android;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class MapFragment extends Fragment {
+
+    View v;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        v = inflater.inflate(R.layout.mapfragment, container, false);
+        return v;
+    }
+
+}
